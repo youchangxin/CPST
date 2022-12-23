8fa0e2a4f067a850587822c2709a2b72fa4ed2f9
youchangxin <423884489@qq.com>
2022-12-23 10:24:11 +0800

    solve save model problem

--- models/base_model.py
+++ models/base_model.py
@@ -174,7 +174,7 @@ class BaseModel(ABC):
                 net = getattr(self, name)
 
                 if len(self.gpu_ids) > 0 and torch.cuda.is_available():
-                    torch.save(net.model.cpu().state_dict(), save_path)
+                    torch.save(net.cpu().state_dict(), save_path)
                     net.cuda(self.gpu_ids[0])
                 else:
                     torch.save(net.cpu().state_dict(), save_path)
